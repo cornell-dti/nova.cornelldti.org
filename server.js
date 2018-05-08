5a8a622fa57a2c2aa1657394edfe93f21b43e1b1
rockon999 <rockon999@users.noreply.github.com>
2018-05-07 22:05:46 -0400

    Implement circle progress. - More single quoting.

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ var path = require('path');
 var serveStatic = require('serve-static');
 
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
+app.use(serveStatic(__dirname + '/dist'));
 
 var port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log('Server started on: '+ port + '!');
+console.log('Server started on: ' + port + '!');
--- src/components/CircleProgressIndicator.vue
+++ src/components/CircleProgressIndicator.vue
@@ -1,33 +1,75 @@
 <template>
-    <div class="circle-progress">
-        <div class="circle">
-            <div class="mask full">
-                <div class="fill" />
+    <div class="circle-progress" style="z-index: 102;">
+        <div class="circle" style="z-index: 101;">
+            <div class="mask full" style="z-index: 100;">
+
+                <div class="fill" style="z-index: 1;" />
+                <div class="fill-hider" style="z-index: 2;" />
+                <div class="fill" style="z-index: 3;" />
+                <div class="fill-hider" style="z-index: 4;" />
             </div>
         </div>
-        <div class="inset" />
+        <div class="inset" style="z-index: 200;" />
     </div>
 </template>
 
 <script>
 export default {
+  props: {
+    percentage: {
+      type: Number,
+      default: 0.0
+    }
+  },
+  mounted() {
+    this.setPercentage(this.percentage);
+  },
   methods: {
-    setPercentage() {
-      const rotation = Math.floor(Math.random() * 360);
+    // todo ensure I'm selecting the right z-indexed div (use ids/classes instead of finger crossing)
 
-      Array.from(document.querySelectorAll('.circle .fill')).forEach(
-        element => {
-          // element.style["-webkit-transform"] = ` rotate(${rotation})`;
-          // element.style.transform = `rotate(${rotation})`;
+    setPercentage(percentage = 0.5) {
+      let percentageDeg = Math.round(percentage * 360);
 
-          element.setAttribute(
-            'style',
-            `${
-              element.style
-            } -webkit-transform: rotate(${rotation}); transform: rotate(${rotation});`
-          );
-        }
-      );
+      const fillHiders = Array.from(document.querySelectorAll('.fill-hider'));
+      const fillers = Array.from(document.querySelectorAll('.fill'));
+
+      // todo convert some to actual css
+
+      if (percentageDeg >= 180) {
+        let e = fillers[0];
+        e.style.transform = `rotate(-180deg)`;
+        e = fillers[1];
+        e.style.transform = `rotate(0deg)`;
+
+        let element = fillHiders[0];
+
+        element.style['-webkit-transform'] = ` rotate(${-percentageDeg}deg)`;
+        element.style.transform = `rotate(${-percentageDeg}deg)`;
+
+        element = fillHiders[1];
+
+        element.style.visibility = 'hidden';
+      } else {
+        let e = fillers[0];
+
+        e.style.transform = `rotate(0deg)`;
+        e.style.clip = 'rect(0px, 119px, 59px, 0px)';
+
+        e = fillers[1];
+
+        e.style.transform = 'rotate(-180deg)';
+        e.style.clip = 'rect(0px, 119px, 59px, 0px)';
+
+        let element = fillHiders[0];
+
+        element.style['-webkit-transform'] = ` rotate(${-percentageDeg}deg)`;
+        element.style.transform = `rotate(${-percentageDeg}deg)`;
+
+        element = fillHiders[1];
+
+        element.style['-webkit-transform'] = ` rotate(-180deg)`;
+        element.style.transform = `rotate(-180deg)`;
+      }
     }
   }
 };
@@ -57,23 +99,31 @@ $inset-color: #fefefe;
     border-radius: 50%;
   }
   .circle {
-    mask {
+    .mask {
       clip: rect(0px, $circle-size, $circle-size, $circle-size / 2);
 
       .fill {
-        clip: rect(0px, $circle-size/2, $circle-size, 0px);
+        background-color: $circle-fill-color;
+        width: $circle-size;
+        height: $circle-size;
+        border-radius: 50%;
+        position: absolute;
+        clip: rect(0px, $circle-size, $circle-size / 2, 0px);
+        -webkit-backface-visibility: hidden;
+        backface-visibility: hidden;
+      }
+
+      .fill-hider {
+        background-color: $circle-background-color;
+        width: $circle-size;
+        height: $circle-size;
+        border-radius: 48.5%;
+        position: absolute;
+        clip: rect(0px, $circle-size, $circle-size / 2, 0px);
+        -webkit-backface-visibility: hidden;
+        backface-visibility: hidden;
       }
     }
-  }
-  .circle .fill {
-    background-color: $circle-fill-color;
-    width: $circle-size;
-    height: $circle-size;
-    border-radius: 50%;
-    position: absolute;
-    clip: rect(0px, $circle-size / 2, $circle-size, 0px);
-    -webkit-backface-visibility: hidden;
-    backface-visibility: hidden;
   }
 }
 </style>
