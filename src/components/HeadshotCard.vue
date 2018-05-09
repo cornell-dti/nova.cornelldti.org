a318df7c329d7f94fc0b7bbfa7ab0596c2b9ad5b
rockon999 <rockon999@users.noreply.github.com>
2018-05-08 23:03:22 -0400

    More backed headcard styling.

--- src/components/HeadshotCard.vue
+++ src/components/HeadshotCard.vue
@@ -1,19 +1,23 @@
 <template>
   <b-container fluid>
-    <b-row class="headshot-card" no-gutters=true>
+    <b-row class="headshot-card no-gutters">
       <b-col>
-        <b-row no-gutters=true>
+        <b-row class="image-row no-gutters">
           <b-col>
             <img :src="image" />
           </b-col>
         </b-row>
-        <b-row class="info" no-gutters=true>
+        <b-row class="info no-gutters">
           <b-col>
-            <b-row no-gutters=true align-v="top">
-              <h5 class="name">{{name}}</h5>
+            <b-row class="h-75 no-gutters" align-v="start">
+              <b-col align-self="start">
+                <div class="name">{{name}}</div>
+              </b-col>
             </b-row>
-            <b-row no-gutters=true align-v="bottom">
-              <p class="role" v-if="role !== ''">{{role}}</p>
+            <b-row class="h-25 no-gutters" align-v="fill">
+              <b-col align-self="end">
+                <div class="role" v-if="role !== ''">{{role}}</div>
+              </b-col>
             </b-row>
           </b-col>
         </b-row>
@@ -50,29 +54,43 @@ export default {
 .headshot-card {
   margin: 1vw;
   text-align: left;
-  width: 12em;
-  max-width: 12em;
-  max-height: 18.5em;
- height: 18.5em;
+  width: 11em;
+  max-height: 17em;
+  height: 17em;
   box-shadow: 0 0 4px 2px rgba(0, 0, 0, 0.2);
 
   .info {
-    padding: 1.1em;
-    height: 6.5em;
+    padding: 0.75em;
+    height: 40%;
+    flex-grow: 1;
 
     .name {
       margin-top: 0;
+      font-size: 1.55em;
+      line-height: 1em;
     }
 
     .role {
       margin: 0;
+      font-size: 0.8em;
+      line-height: 0.8em;
+      color: #777;
     }
+
+    .role-row {
+      flex-grow: 1;
+    }
+  }
+
+  .image-row {
+    height: 59.5%;
+    overflow: hidden;
   }
 
   img {
     object-fit: cover;
     width: 100%;
-    height: 12em;
+    height: 100%;
     background-color: #d8d8d8;
   }
 }
