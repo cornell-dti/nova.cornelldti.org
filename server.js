596c69279e9ee21cab5f8525d30ecf99613b74d8
Shea Belsky <shb95@cornell.edu>
2018-05-12 01:19:27 -0400

    Fixing Apply, Express server

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express     = require("express");
+const history     = require('connect-history-api-fallback');
 const mailchimp   = require("mailchimp-node")(process.env.mailchimpkey);
 const morgan      = require("morgan");
 const path        = require("path");
 
 const app = express();
 require("dotenv").load();
-var express = require('express');
-var path = require('path');
-var history = require('connect-history-api-fallback');
 
-app = express();
 app.use(history());
 app.use(express.static(__dirname + '/dist'));
 
@@ -20,4 +17,3 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server started on port ${port}!`);
 });
-console.log('Server started on: ' + port + '!');
