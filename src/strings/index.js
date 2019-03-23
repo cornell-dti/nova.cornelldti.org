e6c603e126deb4d94f3ebb34245dd5bcc1d76ec4
Will Spencer <wes229@cornell.edu>
2019-03-23 16:31:22 -0400

    Copy strings folder

---
+++ src/strings/index.js
@@ -0,0 +1,7 @@
+// todo
+import JSONStringsBackend from './jsonStringsBackend';
+
+const SingleBackend = new JSONStringsBackend();
+
+// export one StringsFrontend instance
+export default SingleBackend;
