4cf3423382e5f098d06a0b8479442ead2705577a
Will Spencer <wes229@cornell.edu>
2019-09-17 02:04:59 -0400

    Footer redesign (#51)
    * Desktop and mobile implementation of the new footer design
    
    * Updated wordmark and fixed footer spacing
    
    * Updated social media icons
    
    * Added clear contact buttons
    
    * Add mailchimp form and vue class components.

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-require('dotenv').load();
+if (process.env.environment !== 'production') {
+  console.log('Development environment detected, loading environment from available .env files.');
+  require('dotenv').load();
+}
 
 const bodyParser = require('body-parser');
 const express = require('express');
 const history = require('connect-history-api-fallback');
-const Mailchimp = require('mailchimp-api-v3');
 const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
-const mailchimp = new Mailchimp(process.env.mailchimpkey);
 
 if (process.env.environment === 'production') {
   console.log('Production environment detected, enforcing HTTPS & SSL.');
@@ -27,37 +28,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const port = process.env.PORT || 5001;
-
-app.post('/email', (req, res) => {
-  //TODO: Validate email address
-  //TODO: Convert List ID to environment variable
-  const email = req.body.email;
-  mailchimp
-    .post({
-      path: `/lists/839cc09118`,
-      body: {
-        members: [
-          {
-            email_address: email,
-            status: 'subscribed'
-          }
-        ]
-      }
-    })
-    .then(result => {
-      return res.status(200).json({
-        success: true,
-        msg: 'Successfully subscribed!'
-      });
-    })
-    .catch(error => {
-      console.log(error);
-      return res.status(500).json({
-        success: false,
-        msg: error
-      });
-    });
-});
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}!`);
