bf956f7ca51f79ac587f47c0dfcfb69d308653f4
Evan Welsh <2943271+rockon999@users.noreply.github.com>
2019-09-17 00:59:44 -0400

    Remove unnessary email endpoint.

--- server.js
+++ server.js
@@ -6,12 +6,8 @@ if (process.env.environment !== 'production') {
 const bodyParser = require('body-parser');
 const express = require('express');
 const history = require('connect-history-api-fallback');
-const Mailchimp = require('mailchimp-api-v3');
 const morgan = require('morgan');
 const path = require('path');
-
-const mailchimp = new Mailchimp(process.env.mailchimp_key);
-const mailchimp_path = `/lists/${process.env.mailchimp_audience}`;
 
 const app = express();
 
@@ -32,41 +28,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const port = process.env.PORT || 5001;
-
-app.post('/email', (req, res) => {
-  //TODO: Validate email address
-  const { email } = req.body;
-
-  mailchimp
-    .post({
-      path: mailchimp_path,
-      body: {
-        tags: [
-          'Monthly Newsletter'
-        ],
-        members: [
-          {
-            email_address: email,
-            status: 'subscribed'
-          }
-        ]
-      }
-    })
-    .then(() => {
-      return res.status(200).json({
-        success: true,
-        msg: 'Successfully subscribed!'
-      });
-    })
-    .catch(error => {
-      console.log(error);
-
-      return res.status(500).json({
-        success: false,
-        msg: error
-      });
-    });
-});
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}!`);
