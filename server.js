cf7e68327c9c747c5f23d38033c45d33e1493cd2
Evan Welsh <2943271+rockon999@users.noreply.github.com>
2019-09-29 18:16:41 -0400

    Separate public and static files.

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(history());
 app.use(morgan('dev'));
 
 app.use(express.static(path.join(__dirname, '/dist')));
-app.use('/public', express.static(path.join(__dirname, '/public')));
+app.use('/public', express.static(path.join(__dirname, '/static')));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
