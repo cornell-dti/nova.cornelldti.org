f389897fa959231f4b12ea1df7a4e5a711e604bc
Sam <sam@developersam.com>
2019-11-25 22:27:46 -0500

    Type annotate default export in preview.ts

--- cms/preview.ts
+++ cms/preview.ts
@@ -1,3 +1,4 @@
+import { Component, FunctionalComponentOptions } from 'vue';
 import { StringsFrontendPreview } from '@/data/strings/strings';
 import { JSONStringsBackendPreview } from '@/data/strings/jsonStringsBackend';
 
@@ -5,7 +6,15 @@ export function entryToStrings(entry) {
   return entry.getIn(['data']).toJS();
 }
 
-export default (name, Widget, loadStrings = true) => ({
+interface Props {
+  entry: { required: true };
+}
+
+export default (
+  name: string,
+  Widget: Component,
+  loadStrings = true
+): FunctionalComponentOptions<Props> => ({
   name: `${name}Preview`,
   props: {
     entry: {
@@ -43,6 +52,8 @@ export default (name, Widget, loadStrings = true) => ({
       },
       { attrs: { id: `${name}Preview` } },
       cx.data,
+      // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+      // @ts-ignore
       cx.children
     );
   }
