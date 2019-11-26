2479b66c263f57b5a92a7595c8626bb9b94dc27e
Sam <sam@developersam.com>
2019-11-25 22:25:54 -0500

    Type variables in createAsyncPage stricter

--- src/async.ts
+++ src/async.ts
@@ -1,4 +1,9 @@
-import { FunctionalComponentOptions, EsModuleComponent } from 'vue/types/options';
+import {
+  FunctionalComponentOptions,
+  EsModuleComponent,
+  AsyncComponentFactory
+} from 'vue/types/options';
+import { CreateElement } from 'vue';
 import StringsFrontend from '@/data/strings/strings';
 import SingleBackend from '@/data/strings/lib';
 import ErrorComponent from '@/components/ErrorComponent.vue';
@@ -9,7 +14,9 @@ function createAsyncPage(
   pageComponent: () => Promise<EsModuleComponent>
 ): Promise<FunctionalComponentOptions> {
   const StringInstance = new StringsFrontend(context, SingleBackend);
-  const AsyncHandler = () => ({
+  const AsyncHandler: AsyncComponentFactory = () => ({
+    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+    // @ts-ignore
     component: StringInstance.initialize()
       .then(() => pageComponent())
       .then(cmp => {
@@ -27,7 +34,7 @@ function createAsyncPage(
   });
   return Promise.resolve({
     functional: true,
-    render(h: (handler: any, data: any, children: any) => any, { data, children }) {
+    render(h: CreateElement, { data, children }) {
       return h(AsyncHandler, data, children);
     }
   });
