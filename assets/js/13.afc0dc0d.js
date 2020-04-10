48585aff6b505ec1050bdc4e00c4cc791bcd1f71
SamChou19815 <SamChou19815@users.noreply.github.com>
2020-04-10 21:19:02 +0000

    Automated deployment: Fri Apr 10 21:19:02 UTC 2020 bb54f5ffaaffc1dbdb964f60ca24af04f65ceb71

---
+++ assets/js/13.afc0dc0d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{333:function(e){e.exports=JSON.parse('{"roles":[{"id":"designer","name":"Design"},{"id":"developer","name":"Development"},{"id":"business","name":"Business"},{"id":"pm","name":"Product"}]}')}}]);
