1e23ad7c5309dd4f7e9a4d99653ac59ae8029755
Sam <sam@developersam.com>
2020-05-20 00:14:18 -0400

    Remove unused `strings` folder
    This is the leftover code before we have gridsome.

--- packages/website/src/strings/json.ts
+++
@@ -1,9 +0,0 @@
-import StringsFrontend from './strings';
-import SingleBackend from './lib';
-
-// eslint-disable-next-line import/prefer-default-export
-export function fromJSON(id: string, json: unknown) {
-  const StringInstance = new StringsFrontend(id, SingleBackend);
-  StringInstance.initialize(json);
-  return StringInstance;
-}
