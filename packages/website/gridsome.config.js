57ecac1c83d953c37009fed3c59051a6a88aa077
Evan Welsh <rockon999@users.noreply.github.com>
2020-05-23 00:19:54 -0500

    Avoid using entries for pages, prefer the filesystem API. (#290)
    - Let's just simplify this all and avoid GraphQL wherever possible.
    
    Co-authored-by: Evan Welsh <2943271+rockon999@users.noreply.github.com>

--- packages/website/gridsome.config.js
+++ packages/website/gridsome.config.js
@@ -9,46 +9,6 @@ module.exports = {
   templates: {
     DTIProject: '/projects/:teamId_raw',
     Member: '/members/:netid_raw',
-    HomeEntry: [{
-      name: 'Home',
-      path: '/',
-      component: './src/entries/Home.vue'
-    }],
-    TeamEntry: [{
-      name: 'Team',
-      path: '/team',
-      component: './src/entries/Team.vue'
-    }],
-    InitiativesEntry: [{
-      name: 'Initiatives',
-      path: '/initiatives',
-      component: './src/entries/Initiatives.vue'
-    }],
-    PrivacyEntry: [{
-      name: 'Privacy',
-      path: '/privacy',
-      component: './src/entries/Privacy.vue'
-    }],
-    ProjectsEntry: [{
-      name: 'Projects',
-      path: '/projects',
-      component: './src/entries/Projects.vue'
-    }],
-    SponsorEntry: [{
-      name: 'Sponsor',
-      path: '/sponsor',
-      component: './src/entries/Sponsor.vue'
-    }],
-    CoursesEntry: [{
-      name: 'Courses',
-      path: '/courses',
-      component: './src/entries/Courses.vue'
-    }],
-    ApplyEntry: [{
-      name: 'Apply',
-      path: '/apply',
-      component: './src/entries/Apply.vue'
-    }],
   },
   plugins: [
     {
@@ -70,62 +30,6 @@ module.exports = {
       options: {
         typeName: 'Member',
         path: './data/generated/members/*.json',
-      },
-    },
-    {
-      use: '@gridsome/source-filesystem',
-      options: {
-        typeName: 'HomeEntry',
-        path: './data/pages/home.json',
-      },
-    },
-    {
-      use: '@gridsome/source-filesystem',
-      options: {
-        typeName: 'TeamEntry',
-        path: './data/pages/team.json',
-      },
-    },
-    {
-      use: '@gridsome/source-filesystem',
-      options: {
-        typeName: 'CoursesEntry',
-        path: './data/pages/courses.json',
-      },
-    },
-    {
-      use: '@gridsome/source-filesystem',
-      options: {
-        typeName: 'InitiativesEntry',
-        path: './data/pages/initiatives.json',
-      },
-    },
-    {
-      use: '@gridsome/source-filesystem',
-      options: {
-        typeName: 'ProjectsEntry',
-        path: './data/pages/projects.json',
-      },
-    },
-    {
-      use: '@gridsome/source-filesystem',
-      options: {
-        typeName: 'PrivacyEntry',
-        path: './data/pages/privacy.json',
-      },
-    },
-    {
-      use: '@gridsome/source-filesystem',
-      options: {
-        typeName: 'SponsorEntry',
-        path: './data/pages/sponsor.json',
-      },
-    },
-    {
-      use: '@gridsome/source-filesystem',
-      options: {
-        typeName: 'ApplyEntry',
-        path: './data/pages/apply.json',
       },
     },
     {
