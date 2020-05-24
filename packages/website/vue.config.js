6b9943364de169c9fdbd72dbcc80ca44c46c3947
Sam <sam@developersam.com>
2020-05-24 18:21:50 -0400

    Add back config for svg loader

--- packages/website/vue.config.js
+++ packages/website/vue.config.js
@@ -1,1 +1,15 @@
-module.exports = {};
+module.exports = {
+  chainWebpack: config => {
+    // Copied from https://github.com/visualfanatic/vue-svg-loader#vue-cli
+    const svgRule = config.module.rule('svg');
+
+    svgRule.uses.clear();
+
+    svgRule
+      .use('babel-loader')
+      .loader('babel-loader')
+      .end()
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader');
+  }
+};
