05e4be598bd0ec0e42bd9af4add57cb14b3aea6e
Sam Zhou <sam@developersam.com>
2020-05-24 18:35:44 -0400

    Remove unused dependencies (#302)
    * Remove unused dependencies
    
    A lot of dependencies are unused, they seem to be a left over of the old Vue CLI
    
    - postcss related: [Vue CLI enables them by default](https://cli.vuejs.org/guide/css.html#postcss)
    - corejs: explicit version is no longer needed.
    - eslint-import-resolver-webpack, eslint-config-airbnb-base: unused in the eslint config.
    - Babel dependencies related to typescript support for Vue: Vue CLI has good support now, so we can remove our custom tweaks.
    - eslint-plugin-import: Already a dependency of Vue's eslint plugins.

--- packages/website/vue.config.js
+++ packages/website/vue.config.js
@@ -1,38 +1,15 @@
 module.exports = {
-  chainWebpack: (config) => {
-    config.resolve.extensions
-      .add('.tsx')
-      .add('.ts');
-
+  chainWebpack: config => {
+    // Copied from https://github.com/visualfanatic/vue-svg-loader#vue-cli
     const svgRule = config.module.rule('svg');
 
     svgRule.uses.clear();
 
     svgRule
-      .use('vue-svg-loader')
-      .loader('vue-svg-loader');
-
-    // We'll handle tsx ourselves, thank you.
-    config.module.rule('ts').uses.clear();
-    config.module.rule('tsx').uses.clear();
-
-    config.module.rule('typescript').uses.clear();
-
-    config.module
-      .rule('typescript')
-      .test(/\.tsx?$/)
       .use('babel-loader')
       .loader('babel-loader')
-      .options({
-        presets: [
-          '@vue/babel-preset-app'
-        ]
-      })
       .end()
-      .use('ts-loader')
-      .loader('ts-loader')
-      .options({ appendTsSuffixTo: [/\.vue$/] })
-      .end()
-      .end();
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader');
   }
-}
+};
